7fde97ec9720b5889a6ec52411e504e572963fd4
Enzo-Minardi <enzonzmdya@gmail.com>
2025-04-23 00:01:37 -0300

    Adicionando todas entregas

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FECAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
